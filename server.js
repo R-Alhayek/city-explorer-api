563535b4c3122a907c3ea05b76bccb83c96c9f7b
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-07-08 01:18:29 +0300

    finished lab 8

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const axios = require('axios');
 
 
 const weatherData = require('./data/weather.json');
@@ -21,32 +22,73 @@ server.get('/test', (req, res) => {
     res.status(200).send('my server is working')
 })
 
-server.get('/weather', (req, res) => {
-    const weatherInfo = weatherData.find(city => {
-        if (city.city_name == req.query.city) {
-            // console.log('found');
-            return city;
-        } else {
-            return ('Error, your city name is not found');
-        }
+
+server.get('/weather', forcastHandler);
+
+function forcastHandler(request,response) {
+    const sQuery = request.query.city
+
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${sQuery}&key=${process.env.WEATHER_API_KEY}&days=5`
+
+    axios
+    .get(url)
+    .then(weatherData =>{
+        // console.log(weatherData.data);
+        response.status(200).send(weatherData.data.data.map(day =>{
+            return new Forcast(day)
+
+        }))
+    })
+    .catch (error => {
+        response.status(500).send(error)
 
     })
-    let forcastData = weatherInfo.data.map(element => {
-        return new Forcast(`description:${element.weather.description}`, `date:${element.valid_date} `);
-    })
-    res.status(200).send(forcastData);
-
-})
-
-class Forcast {
-    constructor(description, date) {
-        this.description = description,
-        this.date = date
-       
-    }
-
 }
 
+
+server.get('/movies', moviesHandler);
+
+function moviesHandler(request,response) {
+    const sQuery = request.query.city
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${sQuery}`
+
+    axios
+    .get(url)
+    .then(moviesData =>{
+        console.log(moviesData.data.results[0].title);
+        response.status(200).send(moviesData.data.results.map( movie =>{
+            return new Movie(movie)
+
+        }))
+    })
+    .catch (error => {
+        response.status(500).send(error)
+
+    })
+}
+
+class Forcast {
+    constructor(day){
+        this.desc = day.weather.description,
+        this.date = day.valid_date
+
+    }
+}
+
+
+class Movie {
+    constructor(movie){
+        this.title = movie.original_title;
+        this.overview = movie.overview;
+        this.average_votes = movie.vote_average;
+        this.total_votes = movie.vote_count;
+        this.image_url = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+        this.popularity = movie.popularity;
+        this.released_on = movie.release_date;
+
+    }
+}
 
 
 
